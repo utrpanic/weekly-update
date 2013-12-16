2270139e5fca2c9c713e293efe2a4d4664bee35c
Hyeongbaek Jeon <utrpanic@hotmail.com>
2013-12-16 23:53:23 +0900

    Alien Numbers - large set 처리하도록 구현.
    - small set은 16진수까지 테스트하므로 Integer.parseInt()를 그냥 사용할 수 있었지만,
    - large set은 94진수까지 테스트하므로 직접 구현해야 했음.

--- AlienNumbers/src/com/weeklyupdate/aliennumbers/AlienNumbers.java
+++ AlienNumbers/src/com/weeklyupdate/aliennumbers/AlienNumbers.java
@@ -1,18 +1,12 @@
 package com.weeklyupdate.aliennumbers;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class AlienNumbers {
-    public static void main(String[] args) throws IOException {
-        // file input
-        BufferedReader in = null;
-        BufferedWriter out = null;
-        try {
-            in = new BufferedReader(new FileReader("AlienNumbers/A-small-practice.in"));
-            out = new BufferedWriter(new FileWriter("AlienNumbers/A-small-practice.out"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    private void process(String inputFileName, String outputFileName) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(inputFileName));
+        BufferedWriter out = new BufferedWriter(new FileWriter(outputFileName));
 
         int testCaseCount = Integer.parseInt(in.readLine());
         String alienNumber, sourceLanguage, targetLanguage;
@@ -26,31 +20,59 @@ public class AlienNumbers {
 
             String targetString = translate(alienNumber, sourceLanguage, targetLanguage);
 
-//            String outputLine = i + ": " + alienNumber + " " + sourceLanguage + "  " + targetLanguage + "\n";
-//            System.out.println(outputLine);
-
             out.write("Case #" + (i + 1) + ": " + targetString + "\n");
         }
         in.close();
         out.close();
     }
 
-    private static String translate(String alienNumber, String sourceLanguage, String targetLanguage) {
-        String njin = "";
+    private String translate(String alienNumber, String sourceLanguage, String targetLanguage) {
+        int sourceNumberBase = sourceLanguage.length();
+        int targetNumberBase = targetLanguage.length();
+        int[] numberInBase = new int[alienNumber.length()];
         for (int i = 0; i < alienNumber.length(); i++) {
             char a = alienNumber.charAt(i);
             int n = sourceLanguage.indexOf(a);
-            njin += n;
+            numberInBase[i] = n;
         }
-        System.out.println(njin + ": " + Integer.parseInt(njin, sourceLanguage.length()));
-        int decimalNumber = Integer.parseInt(njin, sourceLanguage.length());
-        String targetNumber = Integer.toString(decimalNumber, targetLanguage.length());
-        njin = "";
-        for (int i = 0; i < targetNumber.length(); i++) {
-            char a = targetNumber.charAt(i);
-            int n = Integer.parseInt(String.valueOf(a));
-            njin += targetLanguage.charAt(n);
+
+        int decimalNumber = parseToDecimalNumber(numberInBase, sourceNumberBase);
+        int[] targetNumber = parseToBaseNumber(decimalNumber, targetNumberBase);
+
+        String targetAlienNumber = "";
+        for (int targetNumberAt : targetNumber) {
+            targetAlienNumber += String.valueOf(targetLanguage.charAt(targetNumberAt));
         }
-        return njin;
+        return targetAlienNumber;
+    }
+
+    private int parseToDecimalNumber(int[] numberInBase, int sourceNumberBase) {
+        int decimalNumber = 0;
+        int sourceNumberBaseUnit = 1;
+        for (int i = numberInBase.length - 1; i >= 0; i--) {
+            decimalNumber += numberInBase[i] * sourceNumberBaseUnit;
+            sourceNumberBaseUnit *= sourceNumberBase;
+        }
+        return decimalNumber;
+    }
+
+    private int[] parseToBaseNumber(int decimalNumber, int targetNumberBase) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while (decimalNumber != 0) {
+            list.add(0, decimalNumber % targetNumberBase);
+            decimalNumber /= targetNumberBase;
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        AlienNumbers alienNumbers = new AlienNumbers();
+
+        alienNumbers.process("AlienNumbers/A-small-practice.in", "AlienNumbers/A-small-practice.out");
+        alienNumbers.process("AlienNumbers/A-large-practice.in", "AlienNumbers/A-large-practice.out");
     }
 }
