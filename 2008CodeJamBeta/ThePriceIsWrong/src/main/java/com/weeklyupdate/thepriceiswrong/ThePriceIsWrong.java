0663afea91cc82312dee2a740de5cd9dcf5cd0a6
전형백 <utrpanic@gmail.com>
2014-02-23 23:17:40 +0900

    2014년 2월 23일
    - 마무리 되어가긴 하는데, 땜빵식 코드가 너무 심함. 딱 시험문제를 풀기 위한 코드.
    - 알고리즘을 재정비할 필요가 있음. 현재의 접근 방식을 뭐라고 하더라. dynamic programming?
    - 재귀호출을 진행하기 전에 조건식이 아직 부족함.

--- 2008CodeJamBeta/ThePriceIsWrong/src/main/java/com/weeklyupdate/thepriceiswrong/ThePriceIsWrong.java
+++ 2008CodeJamBeta/ThePriceIsWrong/src/main/java/com/weeklyupdate/thepriceiswrong/ThePriceIsWrong.java
@@ -1,11 +1,15 @@
 package com.weeklyupdate.thepriceiswrong;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ThePriceIsWrong {
 
-    String[] orders;
-    int[] guesses;
+    String[] products;
+    double[] guesses;
+
+    ArrayList<String> changedGuesses;
 
     public static void main(String[] args) throws IOException {
         ThePriceIsWrong.process("ThePriceIsWrong/B-small-practice.in", "ThePriceIsWrong/B-small-practice.out");
@@ -23,29 +27,106 @@ public class ThePriceIsWrong {
             thePriceIsWrong.initGuess(in.readLine());
             String output = "Case #" + (i + 1) + ": " + thePriceIsWrong.getOutput() + "\n";
             out.write(output);
-            System.out.print(output);
+            System.out.print(output + "\n");
         }
 
         in.close();
         out.close();
     }
 
-    void initOrder(String orders) {
-        this.orders = orders.split(" ");
-        System.out.println(orders);
+    public ThePriceIsWrong() {
+
+    }
+
+    public ThePriceIsWrong(ThePriceIsWrong thePriceIsWrong) {
+        this.products = thePriceIsWrong.products;
+        this.guesses = Arrays.copyOf(thePriceIsWrong.guesses, thePriceIsWrong.guesses.length);
+    }
+
+    void initOrder(String products) {
+        this.products = products.split(" ");
+        System.out.println(products);
     }
 
     void initGuess(String guesses) {
         System.out.println(guesses);
         String[] guessesInString = guesses.split(" ");
-        this.guesses = new int[guessesInString.length];
+        this.guesses = new double[guessesInString.length];
         for (int i = 0; i < guessesInString.length; i++) {
             this.guesses[i] = Integer.parseInt(guessesInString[i]);
         }
     }
 
     String getOutput() {
-        return "bar code";
+        rearrangeGuesses(0);
+        return getChangedGuessesString();
+    }
+
+    String getChangedGuessesString() {
+        String[] changedGuessesArray = new String[0];
+        changedGuessesArray = changedGuesses.toArray(changedGuessesArray);
+        Arrays.sort(changedGuessesArray);
+        StringBuilder output = new StringBuilder();
+        for (String changedGuess : changedGuessesArray) {
+            output.append(changedGuess).append(" ");
+        }
+        return output.toString().trim();
+    }
+
+    private void rearrangeGuesses(int start) {
+        changedGuesses = new ArrayList<String>();
+        for (int i = start; i < products.length - 1; i++) {
+            if (guesses[i] > guesses[i + 1]) {
+                if (i == start && i != 0) {
+                    ThePriceIsWrong changeNext = new ThePriceIsWrong(this);
+                    changeNext.guesses[i + 1] = changeNext.guesses[i] + 0.01;
+                    changeNext.rearrangeGuesses(i + 1);
+                    changedGuesses = changeNext.changedGuesses;
+                    changedGuesses.add(products[i + 1]);
+                } else {
+                    ThePriceIsWrong changeCurrent = new ThePriceIsWrong(this);
+                    changeCurrent.guesses[i] = changeCurrent.guesses[i + 1] - 0.01;
+                    changeCurrent.rearrangeGuesses(i + 1);
+                    ThePriceIsWrong changeNext = new ThePriceIsWrong(this);
+                    changeNext.guesses[i + 1] = changeNext.guesses[i] + 0.01;
+                    changeNext.rearrangeGuesses(i + 1);
+
+                    if (changeCurrent.changedGuesses.size() < changeNext.changedGuesses.size()) {
+                        changedGuesses = changeCurrent.changedGuesses;
+                        changedGuesses.add(products[i]);
+                    } else if (changeCurrent.changedGuesses.size() > changeNext.changedGuesses.size()) {
+                        changedGuesses = changeNext.changedGuesses;
+                        changedGuesses.add(products[i + 1]);
+                    } else {
+                        if (changeCurrent.changedGuesses.size() == 0) {
+                            String changedProduct = products[i].compareTo(products[i + 1]) < 0 ? products[i] : products[i + 1];
+                            changedGuesses.add(changedProduct);
+                        } else {
+                            String current = changeCurrent.getChangedGuessesString();
+                            String next = changeNext.getChangedGuessesString();
+                            if (current.compareTo(next) < 0) {
+                                changedGuesses = changeCurrent.changedGuesses;
+                                changedGuesses.add(products[i]);
+                            } else if (current.compareTo(next) > 0) {
+                                changedGuesses = changeNext.changedGuesses;
+                                changedGuesses.add(products[i + 1]);
+                            } else {
+                                changedGuesses = changeCurrent.changedGuesses;
+                                String changedProduct = products[i].compareTo(products[i + 1]) < 0 ? products[i] : products[i + 1];
+                                changedGuesses.add(changedProduct);
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+    }
+
+    private boolean isCheckedGuess(double guess) {
+        int temp = (int) (guess * 100);
+        return temp % 100 != 0;
     }
 
 
--- 2008CodeJamBeta/ThePriceIsWrong/src/test/java/com/weeklyupdate/thepriceiswrong/ThePriceIsWrongTest.java
+++ 2008CodeJamBeta/ThePriceIsWrong/src/test/java/com/weeklyupdate/thepriceiswrong/ThePriceIsWrongTest.java
@@ -1,5 +1,6 @@
 package com.weeklyupdate.thepriceiswrong;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,10 +9,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ThePriceIsWrongTest {
 
     @Test
+    @Ignore
     public void testCase1() throws Exception {
         ThePriceIsWrong thePriceIsWrong = new ThePriceIsWrong();
         thePriceIsWrong.initOrder("code jam foo bar google");
         thePriceIsWrong.initGuess("20 15 40 30 60");
         assertThat(thePriceIsWrong.getOutput(), is("bar code"));
     }
+
+    @Test
+    @Ignore
+    public void testCase3() throws Exception {
+        ThePriceIsWrong thePriceIsWrong = new ThePriceIsWrong();
+        thePriceIsWrong.initOrder("code jam foo bar google");
+        thePriceIsWrong.initGuess("20 70 40 30 60");
+        assertThat(thePriceIsWrong.getOutput(), is("bar jam"));
+    }
+
+    @Test
+//    @Ignore
+    public void testCase5() throws Exception {
+        ThePriceIsWrong thePriceIsWrong = new ThePriceIsWrong();
+        thePriceIsWrong.initOrder("code jam foo bar google");
+        thePriceIsWrong.initGuess("60 30 40 15 20");
+        assertThat(thePriceIsWrong.getOutput(), is("bar code google"));
+    }
+
 }
