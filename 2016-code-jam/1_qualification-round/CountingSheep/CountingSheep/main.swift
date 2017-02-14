d9521d77c2cd57bcfeed84bb900196a1338ef881
Box Jeon <utrpanic@gmail.com>
2017-02-14 23:37:58 +0900

    swift를 이용해서 시작.

---
+++ 2016-code-jam/1_qualification-round/CountingSheep/CountingSheep/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CountingSheep
+//
+//  Created by gurren-l on 2017. 2. 14..
+//  Copyright © 2017년 boxjeon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
