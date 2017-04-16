6c24e5cb6c19012291bcdc8ae9b1b390034c39ce
Box Jeon <utrpanic@gmail.com>
2017-04-16 21:47:29 +0900

    문제는 푼 것 같은데 large input이 너무 오래 걸림.

--- 2015-code-jam/1-qualification-round/InfiniteHouseOfPancakes/HouseOfPancakes.swift
+++ 2015-code-jam/1-qualification-round/InfiniteHouseOfPancakes/HouseOfPancakes.swift
@@ -8,14 +8,31 @@
 
 class HouseOfPancakes {
     
+    private var time: Int
+    private var pancakes: Array<Int>
+    
     init(input: String) {
-        
+        self.time = 0
+        var inputs = input.components(separatedBy: " ").filter({ !$0.isEmpty })
+        inputs.removeFirst()
+        self.pancakes = inputs.flatMap({ Int($0) })
     }
     
     func output() -> String {
-        
-        
-        return "2"
+        self.pancakes.sort()
+        let max = self.pancakes.last!
+        let countOfMax = pancakes.reduce(0, {
+            return $0 + ($1 == max ? 1 : 0)
+        })
+        if countOfMax <= max / 2 {
+            self.time += 1
+            self.pancakes.removeLast()
+            self.pancakes.append(max / 2)
+            self.pancakes.append(max - (max / 2))
+            return self.output()
+        } else {
+            return String(self.time + max)
+        }
     }
     
 }
--- 2015-code-jam/1-qualification-round/InfiniteHouseOfPancakes/main.swift
+++ 2015-code-jam/1-qualification-round/InfiniteHouseOfPancakes/main.swift
@@ -17,6 +17,7 @@ let tests: Array<Test> = [
     Test(input: "1 3", expected: "3"),
     Test(input: "4 1 2 1 2", expected: "2"),
     Test(input: "1 4", expected: "3"),
+    Test(input: "4 10 9 8 7", expected: "9")
 ]
 
 tests.forEach({
@@ -33,16 +34,21 @@ func generateOutput(practiceName: String) {
     let inputs = (try! String(contentsOfFile: path)).components(separatedBy: "\n").filter({ !$0.isEmpty })
     var outputs = Array<String>()
     for index in stride(from: 1, to: inputs.count, by: 2) {
+        let date = Date()
         let input = inputs[index].appending(" ").appending(inputs[index + 1])
         let result = HouseOfPancakes(input: input).output()
         outputs.append("Case #\(index / 2 + 1): \(result)")
+        let elapsedSec = Date().timeIntervalSince1970 - date.timeIntervalSince1970
+        if elapsedSec > 1 {
+            print("\(practiceName) Case #\(index / 2 + 1): \(elapsedSec) sec.")
+        }
     }
     var outputUrl = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
     outputUrl.appendPathComponent(practiceName)
     outputUrl.appendPathExtension("out")
     try! outputs.joined(separator: "\n").write(to: outputUrl, atomically: true, encoding: .utf8)
+    
 }
 
 generateOutput(practiceName: "B-small-practice")
-
 generateOutput(practiceName: "B-large-practice")
