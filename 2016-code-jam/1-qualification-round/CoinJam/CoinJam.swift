d44e037920a96f95b86c7e75651cf2ea4118f857
Box Jeon <utrpanic@gmail.com>
2017-04-20 18:59:41 +0900

    Large number handling이 필요함. 내게 좋은 생각이 있지.

--- 2016-code-jam/1-qualification-round/CoinJam/CoinJam.swift
+++ 2016-code-jam/1-qualification-round/CoinJam/CoinJam.swift
@@ -6,14 +6,62 @@
 //  Copyright © 2017년 boxjeon. All rights reserved.
 //
 
+import Darwin
+import Foundation
+
 class CoinJam {
     
+    var startCoin: Int
+    let maxCoin: Int
+    let resultCount: Int
+    
     init(input: String) {
-        
+        print(input)
+        let inputs = input.components(separatedBy: " ").flatMap({ Int($0) })
+        self.startCoin = Int(pow(Double(2), Double(inputs[0]) - 1)) + 1
+        self.maxCoin = Int(pow(Double(2), Double(inputs[0])))
+        self.resultCount = inputs[1]
     }
     
     func output() -> String {
-        return ""
+        var results = Array<String>()
+        for coin in stride(from: self.startCoin, to: self.maxCoin, by: 2) {
+            let binaryText = String(coin, radix: 2)
+            var result = binaryText
+            for base in 2 ... 10 {
+                let number = Int(binaryText, radix: base)!
+                print("Number in base \(base): \(number)")
+                if let nontrivialDivisor = self.getNontrivialDivisor(of: number) {
+                    result.append(" \(nontrivialDivisor)")
+                } else {
+                    result = ""
+                    break
+                }
+            }
+            if !result.isEmpty {
+                print("[jamcoin found] \(result)")
+                results.append("\(result)")
+            } else {
+                print("[\(binaryText) is not jamcoin]")
+            }
+            if results.count == self.resultCount {
+                break
+            }
+        }
+        return results.joined(separator: "\n")
+    }
+    
+    func getNontrivialDivisor(of number: Int) -> Int? {
+        if number % 2 == 0 {
+            return 2
+        } else {
+            for divisor in stride(from: 3, to: Int(sqrt(Double(number))), by: 2) {
+                if number % divisor == 0 {
+                    return divisor
+                }
+            }
+            return nil
+        }
     }
     
 }
--- 2016-code-jam/1-qualification-round/CoinJam/main.swift
+++ 2016-code-jam/1-qualification-round/CoinJam/main.swift
@@ -8,24 +8,26 @@
 
 import Foundation
 
-struct Test {
-    var input: String
-    var expected: String
+func verify(output: String) {
+    output.components(separatedBy: "\n").filter({ !$0.isEmpty }).forEach { (jamcoinRecord) in
+        var records = jamcoinRecord.components(separatedBy: " ").filter({ !$0.isEmpty })
+        let jamcoin = records.remove(at: 0)
+        let nontrivalDivisors: Array<Int> = records.flatMap({ Int($0) })
+        for base in 2 ..< 10 {
+            let number = Int(jamcoin, radix: base)!
+            let nontrivialDivisor: Int = nontrivalDivisors[base - 2]
+            let divider = number / nontrivialDivisor
+            let remainder = number % nontrivialDivisor
+            if remainder > 0 || (remainder == 0 && divider == 1) || (remainder == 0 && divider == number) {
+                print("[FAILURE]: \(jamcoinRecord)")
+                return
+            }
+        }
+        print("[success]: \(jamcoinRecord)")
+    }
 }
 
-let tests: Array<Test> = [
-    Test(input: "6 3", expected: "100011 5 13 147 31 43 1121 73 77 629\n111111 21 26 105 1302 217 1032 513 13286 10101\n111001 3 88 5 1938 7 208 3 20 11"),
-]
-
-tests.forEach({
-    print("input: \($0.input)")
-    let output = CoinJam(input: $0.input).output()
-    if $0.expected == output {
-        print("[success]\n\(output)")
-    } else {
-        print("[FAILURE] expected:\n\($0.expected)\nactually: \(output)")
-    }
-})
+verify(output: "100011 5 13 147 31 43 1121 73 77 629\n111111 21 26 105 1302 217 1032 513 13286 10101\n111001 3 88 5 1938 7 208 3 20 11")
 
 func generateOutput(practiceName: String) {
     let path = Bundle.main.path(forResource: practiceName, ofType: "in")!
@@ -34,7 +36,8 @@ func generateOutput(practiceName: String) {
     for index in stride(from: 1, to: inputs.count, by: 1) {
         let input = inputs[index]
         let result = CoinJam(input: input).output()
-        outputs.append("Case #\(index): \(result)")
+        outputs.append("Case #\(index):\n\(result)")
+        verify(output: result)
     }
     var outputUrl = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
     outputUrl.appendPathComponent(practiceName)
