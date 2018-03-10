c1fbab774f3df34c2c65dc46799291d0a1c3a4aa
Box Jeon <utrpanic@gmail.com>
2018-03-10 23:48:08 +0900

    TidyNumbers. Create target.

---
+++ 2017-code-jam/1-qualification-round/TidyNumbers/TidyNumbers.swift
@@ -0,0 +1,20 @@
+//
+//  TidyNumbers.swift
+//  TidyNumbers
+//
+//  Created by boxjeon on 2018. 3. 10..
+//  Copyright © 2018년 boxjeon. All rights reserved.
+//
+
+import Foundation
+
+class TidyNumbers {
+    
+    init(input: String) {
+
+    }
+    
+    func output() -> String {
+        return "129"
+    }
+}
