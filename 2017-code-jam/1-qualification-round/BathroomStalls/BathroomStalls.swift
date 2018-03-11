8d534297933fbaee37715ecb8c2166c982237b14
Box Jeon <utrpanic@gmail.com>
2018-03-12 08:46:25 +0900

    BathroomStalls. 작업 중.

--- 2017-code-jam/1-qualification-round/BathroomStalls/BathroomStalls.swift
+++ 2017-code-jam/1-qualification-round/BathroomStalls/BathroomStalls.swift
@@ -8,11 +8,44 @@
 
 class BathroomStalls {
     
+    var emptyStalls: [Int]
+    let numberOfPeople: Int
+    
     init(input: String) {
-        
+        let inputs = input.split(separator: " ")
+        self.emptyStalls = [Int(inputs[0])!]
+        self.numberOfPeople = Int(inputs[1])!
     }
     
     func output() -> String {
-        return "1 0"
+        var leftStalls: Int = 0
+        var rightStalls: Int = 0
+        for _ in 0 ..< self.numberOfPeople {
+            let stalls = self.retrieveMaxEmptyStalls()
+            if stalls % 2 == 0 {
+                leftStalls = stalls % 2 - 1
+                rightStalls = stalls % 2
+                self.emptyStalls.append(leftStalls)
+                self.emptyStalls.append(rightStalls)
+            } else {
+                leftStalls = stalls % 2
+                rightStalls = stalls % 2
+                self.emptyStalls.append(leftStalls)
+                self.emptyStalls.append(rightStalls)
+            }
+        }
+        return "\(leftStalls) \(rightStalls)"
+    }
+    
+    func retrieveMaxEmptyStalls() -> Int {
+        var maxEmptyStalls: Int = 0
+        var maxEmptyStallsIndex: Int = 0
+        for index in 0 ..< self.emptyStalls.count {
+            if maxEmptyStalls <= self.emptyStalls[index] {
+                maxEmptyStalls = self.emptyStalls[index]
+                maxEmptyStallsIndex = index
+            }
+        }
+        return self.emptyStalls.remove(at: maxEmptyStallsIndex)
     }
 }
