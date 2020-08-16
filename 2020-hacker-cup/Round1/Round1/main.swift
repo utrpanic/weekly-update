00455eedb3bcbf518a35b61b822ea23a7004e4ba
box.jeon <utrpanic@gmail.com>
2020-08-16 20:31:31 +0900

    Workspace Settings.

---
+++ 2020-hacker-cup/Round1/Round1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Round1
+//
+//  Created by gurren-l-macbook-20 on 2020/08/16.
+//  Copyright © 2020 utrpanic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
