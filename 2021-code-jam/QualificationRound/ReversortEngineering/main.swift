fd5e7fd6373a0074a364b617b0bcb75e78cb9fa5
box-jeon <utrpanic@gmail.com>
2021-03-27 13:05:10 +0900

    Code Jam 2021. ReversortEngineering 완료.

--- 2021-code-jam/QualificationRound/ReversortEngineering/main.swift
+++ 2021-code-jam/QualificationRound/ReversortEngineering/main.swift
@@ -1,8 +1,9 @@
 
 final class ReversortEngineering {
     
-    let length: Int
-    let cost: Int
+    let givenCost: Int
+    var totalCost: Int
+    var numbers: [Int]
     
     convenience init() {
         self.init(input: readLine()!)
@@ -12,27 +13,58 @@ final class ReversortEngineering {
         let inputs = input.split(separator: " ")
             .map { String($0) }
             .map { Int($0)! }
-        self.length = inputs[0]
-        self.cost = inputs[1]
+        self.numbers = Array(1...inputs[0])
+        self.givenCost = inputs[1]
+        self.totalCost = inputs[1]
+        
     }
     
     func output() -> String {
-        return "4 2 1 3"
+        let minimumCost = self.numbers.count - 1
+        let maximumCost = (0 ..< self.numbers.count).reduce(0) { $0 + $1 } + minimumCost
+        guard self.givenCost >= minimumCost else { return "IMPOSSIBLE" }
+        guard self.givenCost <= maximumCost else { return "IMPOSSIBLE" }
+        self.totalCost -= minimumCost
+        for index in (0 ..< self.numbers.count - 1).reversed() {
+            self.reverse(at: index)
+            if self.totalCost == 0 {
+                return self.numbers
+                    .map { String($0) }
+                    .joined(separator: " ")
+            }
+        }
+        return "IMPOSSIBLE"
+    }
+    
+    private func reverse(at cursor: Int) {
+        let maxCost = self.numbers.count - cursor - 1
+        let reverseDistance = min(self.totalCost, maxCost)
+        for index in 0 ... (reverseDistance / 2) {
+            let from = cursor + index
+            let to = cursor + (reverseDistance - index)
+            let temp = self.numbers[from]
+            self.numbers[from] = self.numbers[to]
+            self.numbers[to] = temp
+        }
+        self.totalCost -= reverseDistance
     }
 }
 
-//let testCaseCount = Int(readLine()!)!
-//for index in 1 ... testCaseCount {
-//    let output = ReversortEngineering().output()
-//    print("Case #\(index): \(output)")
-//}
+let testCaseCount = Int(readLine()!)!
+for index in 1 ... testCaseCount {
+    let output = ReversortEngineering().output()
+    print("Case #\(index): \(output)")
+}
 
 let testCases: [(String, String)] = [
     ("4 6", "4 2 1 3"),
+    ("2 2", "2 1"),
     ("2 1", "1 2"),
     ("7 12", "7 6 5 4 3 2 1"),
     ("7 2", "IMPOSSIBLE"),
     ("2 1000", "IMPOSSIBLE"),
+    ("100 98", "IMPOSSIBLE"),
+    ("5 10", "5 4 3 2 1"),
 ]
 
 testCases.forEach { (input, expected) in
@@ -41,7 +73,7 @@ testCases.forEach { (input, expected) in
     if result == "IMPOSSIBLE" {
         print(result == expected ? "OK" : result)
     } else {
-        let givenCost = String(reversortEngineering.cost)
+        let givenCost = String(reversortEngineering.givenCost)
         let resultCost = Reversort(input: result).output()
         print(givenCost == resultCost ? "OK" : result)
     }
