bc3e6714e32cd51dc3a68d79a470269a52d512dc
box-jeon <utrpanic@gmail.com>
2021-04-24 22:47:16 +0900

    CodeJam 2017. Bathroom Stalls. stalls 제거. 반복문으로 좌우 빈 방 갯수만 계산.

--- 2017-code-jam/QualificationRound/BathroomStalls/main.swift
+++ 2017-code-jam/QualificationRound/BathroomStalls/main.swift
@@ -1,83 +1,49 @@
 
 class BathroomStalls {
     
-    var stalls: [Bool]
+    let emptyStallCount: Int
     let peopleCount: Int
     
     init(input: String) {
         let inputs = input.split(separator: " ")
-        let stallCount = Int(inputs[0])! + 2
-        var stalls = Array(repeating: false, count: stallCount)
-        stalls[0] = true
-        stalls[stalls.count - 1] = true
-        self.stalls = stalls
+        self.emptyStallCount = Int(inputs[0])!
         self.peopleCount = Int(inputs[1])!
     }
     
     func output() -> String {
-        let lastIndex = self.lastOccupiedStallIndex(start: 0, emptyStallCount: self.stalls.count - 2, peopleCount: self.peopleCount)
-        let result = self.leftRightEmptyStalls(index: lastIndex)
-        let max = Swift.max(result.left, result.right)
-        let min = Swift.min(result.left, result.right)
-        return "\(max) \(min)"
-    }
-    
-    func lastOccupiedStallIndex(start: Int, emptyStallCount: Int, peopleCount: Int) -> Int {
-        let pivot = start + (emptyStallCount + 1) / 2
-        self.stalls[pivot] = true
-        let newEmptyStallCount = emptyStallCount - 1
-        let newPeopleCount = peopleCount - 1
-        if newPeopleCount == 0 {
-            return pivot
-        } else {
-            let leftStart = start
-            let leftEmptyStallCount = pivot - start - 1
-            let leftPeopleCount: Int
-            let rightStart = pivot
-            let rightEmptyStallCount = newEmptyStallCount - leftEmptyStallCount
-            let rightPeopleCount: Int
-            if leftEmptyStallCount == rightEmptyStallCount {
-                leftPeopleCount = (newPeopleCount + 1) / 2
-                rightPeopleCount = newPeopleCount - leftPeopleCount
-                if rightPeopleCount == 0 {
-                    return self.lastOccupiedStallIndex(start: leftStart, emptyStallCount: leftEmptyStallCount, peopleCount: leftPeopleCount)
-                } else {
-                    let leftIndex = self.lastOccupiedStallIndex(start: leftStart, emptyStallCount: leftEmptyStallCount, peopleCount: leftPeopleCount)
-                    let rightIndex = self.lastOccupiedStallIndex(start: rightStart, emptyStallCount: rightEmptyStallCount, peopleCount: rightPeopleCount)
-                    return leftPeopleCount == rightPeopleCount ? rightIndex : leftIndex
-                }
+        var emptyStallCount = self.emptyStallCount
+        var peopleCount = self.peopleCount
+        while true {
+            emptyStallCount -= 1
+            peopleCount -= 1
+            let leftEmptyStallCount = emptyStallCount / 2
+            let rightEmptyStallCount = emptyStallCount - leftEmptyStallCount
+            if peopleCount == 0 {
+                return "\(rightEmptyStallCount) \(leftEmptyStallCount)"
             } else {
-                leftPeopleCount = (newPeopleCount) / 2
-                rightPeopleCount = newPeopleCount - leftPeopleCount
-                if leftPeopleCount == 0 {
-                    return self.lastOccupiedStallIndex(start: rightStart, emptyStallCount: rightEmptyStallCount, peopleCount: rightPeopleCount)
+                if leftEmptyStallCount == rightEmptyStallCount {
+                    let leftPeopleCount = (peopleCount + 1) / 2
+                    let rightPeopleCount = peopleCount - leftPeopleCount
+                    if leftPeopleCount > rightPeopleCount {
+                        emptyStallCount = leftEmptyStallCount
+                        peopleCount = leftPeopleCount
+                    } else {
+                        emptyStallCount = rightEmptyStallCount
+                        peopleCount = rightPeopleCount
+                    }
                 } else {
-                    let leftIndex = self.lastOccupiedStallIndex(start: leftStart, emptyStallCount: leftEmptyStallCount, peopleCount: leftPeopleCount)
-                    let rightIndex = self.lastOccupiedStallIndex(start: rightStart, emptyStallCount: rightEmptyStallCount, peopleCount: rightPeopleCount)
-                    return leftPeopleCount == rightPeopleCount ? leftIndex : rightIndex
+                    let leftPeopleCount = (peopleCount) / 2
+                    let rightPeopleCount = peopleCount - leftPeopleCount
+                    if leftPeopleCount < rightPeopleCount {
+                        emptyStallCount = rightEmptyStallCount
+                        peopleCount = rightPeopleCount
+                    } else {
+                        emptyStallCount = leftEmptyStallCount
+                        peopleCount = leftPeopleCount
+                    }
                 }
             }
         }
-    }
-    
-    func leftRightEmptyStalls(index: Int) -> (left: Int, right: Int) {
-        var left: Int = 0
-        for index in (0 ..< index).reversed() {
-            if self.stalls[index] {
-                break
-            } else {
-                left += 1
-            }
-        }
-        var right: Int = 0
-        for index in (index + 1 ..< self.stalls.count) {
-            if self.stalls[index] {
-                break
-            } else {
-                right += 1
-            }
-        }
-        return (left, right)
     }
 }
 
@@ -105,13 +71,14 @@ let tests: Array<Test> = [
     Test(input: "1000 1", expected: "500 499"),
     Test(input: "999 499", expected: "1 0"),
     Test(input: "1000 489", expected: "1 0"),
+    Test(input: "1000000000000000000 1000000000000000000", expected: "0 0"),
 ]
 
 tests.forEach({
     let output = BathroomStalls(input: $0.input).output()
     if $0.expected == output {
-        print("[SUCCESS] input: \($0.input), output: \(output)")
+        print("✅ input: \($0.input), output: \(output)")
     } else {
-        print("[FAILURE] input: \($0.input), expected: \($0.expected), actually: \(output)")
+        print("❗️ input: \($0.input), expected: \($0.expected), actually: \(output)")
     }
 })
