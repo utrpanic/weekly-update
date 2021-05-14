34068359cc62b95c84fe40ae95120291d008f2d0
box-jeon <utrpanic@gmail.com>
2021-05-14 21:32:21 +0900

    CodeJam2018. TroubleSort 테스트 환경 준비.

--- 2018-code-jam/QualificationRound/TroubleSort/main.swift
+++ 2018-code-jam/QualificationRound/TroubleSort/main.swift
@@ -1,2 +1,50 @@
 
-print("Hello, World!")
+class TroubleSort {
+    
+    var numbers: [Int]
+    
+    convenience init() {
+        _ = readLine()
+        self.init(input: readLine()!)
+    }
+    
+    init(input: String) {
+        self.numbers = input.split(separator: " ").map { Int($0)! }
+    }
+    
+    func output() -> String {
+        return "OK"
+    }
+}
+
+typealias Solution = TroubleSort
+
+func start() {
+    let count = Int(readLine()!)!
+    for index in 1 ... count {
+        let input = readLine()!
+        let solution = Solution(input: input)
+        print("Case #\(index): \(solution.output())")
+    }
+}
+
+//start()
+
+struct Test {
+    var input: String
+    var expected: String
+}
+
+let tests: Array<Test> = [
+    Test(input: "5 6 8 4 3", expected: "OK"),
+    Test(input: "8 9 7", expected: "1"),
+]
+
+tests.forEach({
+    let output = Solution(input: $0.input).output()
+    if $0.expected == output {
+        print("✅ input: \($0.input), output: \(output)")
+    } else {
+        print("❗️ input: \($0.input), expected: \($0.expected), actually: \(output)")
+    }
+})
