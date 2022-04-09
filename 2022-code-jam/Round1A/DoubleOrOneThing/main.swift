a36833d1a3749e80da102afcb7db97b41556084f
box-jeon <utrpanic@gmail.com>
2022-04-09 11:00:48 +0900

    CodeJam 2022. R1A. DoubleOrOneThing. Done.

--- 2022-code-jam/Round1A/DoubleOrOneThing/main.swift
+++ 2022-code-jam/Round1A/DoubleOrOneThing/main.swift
@@ -3,16 +3,36 @@ final class DoubleOrOneThing {
   typealias Input = String
   typealias Output = String
   
+  var input: String
+  
   convenience init() {
     self.init(input: (readLine()!))
   }
   
   init(input: Input) {
-    
+    self.input = input
   }
   
   func output() -> String {
-    return "PEEEEL"
+    var result: [Character] = []
+    let characters = Array(self.input)
+    var sameCharactorCount = 0
+    for index in 0 ..< characters.count {
+      result.append(characters[index])
+      if index + 1 == characters.count { break }
+      if characters[index] == characters[index + 1] {
+        sameCharactorCount += 1
+      } else if characters[index] < characters[index + 1] {
+        sameCharactorCount += 1
+        for _ in 0 ..< sameCharactorCount {
+          result.append(characters[index])
+        }
+        sameCharactorCount = 0
+      } else {
+        sameCharactorCount = 0
+      }
+    }
+    return String(result)
   }
 }
 
